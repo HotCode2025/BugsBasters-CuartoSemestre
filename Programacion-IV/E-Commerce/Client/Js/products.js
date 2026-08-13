d57c834e855d40e65324edde02ac66f2f4bc699c
Brian Sastre <briansastre159@gmail.com>
2026-08-13 11:03:10 -0300

    refactor: corregir nombre de carpeta y crear estructura inicial de E-Commerce - Se corrige el nombre de la carpeta de "programcion IV" a "Programacion-IV". - Se crea la estructura de carpetas `Client`, `Js` y `Media`. - Se agregan los archivos base de la aplicación (index.html, styles.css, products.js e index.js). En la carpeta Client se crea la carpeta media  donde estaran las imagenes

---
+++ Programacion-IV/E-Commerce/Client/Js/products.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        id: 1,
+        name: "bananas",
+        price: 1500,
+        stock: 10,
+        image: "../Media/Images/bananas.jpg"
+    },
+    {
+        id: 2,
+        name: "Leche",
+        price: 2500,
+        stock: 10,
+        image: "../Media/Images/Leche.jpg"
+    },
+    {
+        id: 3,
+        name: "Pollo",
+        price: 7000,
+        stock: 10,
+        image: "../Media/Images/Pollo.jpg"
+    },
+    {
+        id: 4,
+        name: "Mayonesa",
+        price: 2700,
+        stock: 10,
+        image: "../Media/Images/Mayonesa.jpg"
+    },
+    {
+        id: 5,
+        name: "Enlatados",
+        price: 3500,
+        stock: 10,
+        image: "../Media/Images/Enlatados.jpg"
+    },
+    {
+        id: 6,
+        name: "Varios",
+        price: 4500,
+        stock: 10,
+        image: "../Media/Images/Varios.jpg"
+    }
+];
